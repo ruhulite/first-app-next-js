d566a5a186cbb3398e304fe7b84c7c283ddb0c9f
Ruhul <corporate.ruhul@gmail.com>
2019-10-28 14:57:25 +0600

    file add

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d7570801d462b824bf90";
+/******/ 	var hotCurrentHash = "ae7a850495dafcd4482a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
